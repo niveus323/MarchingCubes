f8cc2d41572ce6a6a4238bf2a634e3276b937383
DHlee <niveus323@gmail.com>
2025-10-06 22:56:28 +0900

    Revert "<Feat> : Log 출력 기능 추가, Timer 실행시간 측정 기능 추가"
    This reverts commit cc2db39237d7a5a6a57f65bad67417ecd2d9fb02.

--- MarchingCubes/Core/Utils/Timer.cpp
+++ MarchingCubes/Core/Utils/Timer.cpp
@@ -1,8 +1,5 @@
 #include "pch.h"
 #include "Timer.h"
-#include <string>
-#include <atomic>
-#include "Core/Utils/Log.h"
 
 void Timer::Start()
 {
@@ -15,33 +12,4 @@ float Timer::Tick()
 	std::chrono::duration<float> elapsed = now - m_prev;
 	m_prev = now;
 	return elapsed.count();
-}
-
-void Timer::BeginKey(std::string_view key)
-{
-	std::lock_guard<std::mutex> lock(m_storedKey.mtx);
-	m_storedKey.stackMap[std::string(key)].push_back(clock::now());
-}
-
-double Timer::EndKey(std::string_view key)
-{
-	Time_point t0{};
-	{
-		std::lock_guard<std::mutex> lock(m_storedKey.mtx);
-		auto& vec = m_storedKey.stackMap[std::string(key)];
-		if (vec.empty())
-		{
-			Log::Print("Timer", "Timer::EndKey : key '%.*s' has no active begin", (int)key.size(), key.data());
-			return -1.0f;
-		}
-		t0 = vec.back();
-		vec.pop_back();
-	}
-
-	const auto t1 = clock::now();
-
-	const double ms = std::chrono::duration<double, std::milli>(t1 - t0).count();
-	Log::Print("Timer", "'%.*s' : %.3f ms", (int)key.size(), key.data(), ms);
-
-	return ms;
 }
--- MarchingCubes/Core/Utils/Timer.h
+++ MarchingCubes/Core/Utils/Timer.h
@@ -1,89 +1,12 @@
 #pragma once
 #include <chrono>
-#include <mutex>
-#include <unordered_map>
-#include <vector>
 
 class Timer
 {
-	using clock = std::chrono::steady_clock;
-	using Time_point = clock::time_point;
 public:
 	void Start();
 	float Tick();
 
-	template <typename F, typename... Args>
-	static auto MeasureCall(F&& f, Args&&... args)
-	{
-		auto t0 = clock::now();
-
-		if constexpr (std::is_void_v<std::invoke_result_t<F, Args...>>)
-		{
-			std::invoke(std::forward<F>(f), std::forward<Args>(args)...);
-			auto t1 = clock::now();
-			return std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0);
-		}
-		else
-		{
-			auto ret = std::invoke(std::forward<F>(f), std::forward<Args>(args)...);
-			auto t1 = clock::now();
-			return std::pair{ std::move(ret), std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0) };
-		}
-	}
-	
-	template <typename F, typename... Args>
-	static double MeasureMs(F&& f, Args&&... args)
-	{
-		if constexpr (std::is_void_v<std::invoke_result_t<F, Args...>>) {
-			auto ns = MeasureCall(std::forward<F>(f), std::forward<Args>(args)...);
-			return ns.count() / 1'000'000.0;
-		}
-		else {
-			auto [ret, ns] = MeasureCall(std::forward<F>(f), std::forward<Args>(args)...);
-			(void)ret;
-			return ns.count() / 1'000'000.0;
-		}
-	}
-
-	// 멤버 함수 포인터(비-const/const) 호출 편의 오버로드
-    template <class C, class R, class... MArgs, class Obj, class... Args>
-    static double MeasureMs(R(C::* mf)(MArgs...), Obj&& obj, Args&&... args) {
-        return MeasureMs([&] {
-            std::invoke(mf, std::forward<Obj>(obj), std::forward<Args>(args)...);
-        });
-    }
-    template <class C, class R, class... MArgs, class Obj, class... Args>
-    static double MeasureMs(R(C::* mf)(MArgs...) const, Obj&& obj, Args&&... args) {
-        return MeasureMs([&] {
-            std::invoke(mf, std::forward<Obj>(obj), std::forward<Args>(args)...);
-        });
-    }
-
-#ifdef _WIN32
-	template <typename F, typename... Args>
-	static double MeasureMsQPC(F&& f, Args&&... args)
-	{
-		LARGE_INTEGER freq{}, t0{}, t1{};
-		QueryPerformanceFrequency(&freq);
-		QueryPerformanceCounter(&t0);
-
-		std::invoke(std::forward<F>(f), std::forward<Args>(args)...);
-
-		QueryPerformanceCounter(&t1);
-		return (t1.QuadPart - t0.QuadPart) * 1000.0 / static_cast<double>(freq.QuadPart);
-	}
-#endif // _WIN32
-
-	static void BeginKey(std::string_view key);
-	static double EndKey(std::string_view key);
-
 private:
-	Time_point m_prev;
-
-	struct TimerKey
-	{
-		std::mutex mtx;
-		std::unordered_map<std::string, std::vector<std::chrono::steady_clock::time_point>> stackMap;
-	};
-	inline static TimerKey m_storedKey{};
+	std::chrono::steady_clock::time_point m_prev;
 };
